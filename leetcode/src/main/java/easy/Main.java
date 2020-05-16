a582d6901aa93d9becb5f4a169484e6da4ce496d
yanglulu <yanglu_u@126.com>
2020-05-16 20:48:22 +0800

    add leetcode module

---
+++ leetcode/src/main/java/easy/Main.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer i = new Integer(129);
+        int j = 129;
+        System.out.println(j == i);
+    }
+
+}
